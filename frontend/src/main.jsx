85d8975c0d8e45846bfc6bf7b7a44fbb95de1ba9
rakshitha-code233 <rakshitha2081rp@gmail.com>
2026-04-19 16:44:21 +0530

    Unregister all service workers and clear caches on app load

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Unregister any existing service workers to prevent blank page on refresh
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister())
+  })
+  caches.keys().then((keys) => keys.forEach((key) => caches.delete(key)))
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
